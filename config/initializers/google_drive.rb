e58ee925d12f9cbbcfe1b472a33273ceb9b63db4
matinou-github <hadilouidrissou@gmail.com>
2024-12-31 14:19:15 +0100

    fix again key

--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -3,10 +3,24 @@ require 'googleauth'
 require 'json'
 require 'dotenv/load' # Charge les variables depuis .env
 
+# Charge les informations d'identification JSON depuis la variable d'environnement
 credentials = JSON.parse(ENV['GOOGLE_DRIVE_CREDENTIALS'])
 
+# Crée un service Google Drive
 DRIVE_SERVICE = Google::Apis::DriveV3::DriveService.new
+
+# Autorisation via les informations d'identification de service
 DRIVE_SERVICE.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: StringIO.new(credentials.to_json),
-  scope: ['https://www.googleapis.com/auth/drive']
+  json_key_io: StringIO.new(credentials.to_json),  # Passe les informations d'identification sous forme de JSON
+  scope: ['https://www.googleapis.com/auth/drive']  # Définissez les autorisations requises
 )
+
+# Test pour vérifier la connexion
+begin
+  # Demander la liste des fichiers pour vérifier si l'autorisation fonctionne
+  response = DRIVE_SERVICE.list_files(page_size: 10)
+  puts "Fichiers récupérés avec succès:"
+  response.files.each { |file| puts "#{file.name} (#{file.id})" }
+rescue Google::Apis::Error => e
+  puts "Erreur lors de l'appel à l'API Google Drive: #{e.message}"
+end
