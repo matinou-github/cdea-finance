91f19ed99905174020f5302d208e338628a8222f
matinou-github <hadilouidrissou@gmail.com>
2024-12-31 13:48:50 +0100

    fix drive secure key

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'csv'
 
 gem 'google-api-client', '~> 0.53.0'
 gem 'google_drive', '~> 3.0'
+gem 'dotenv-rails', groups: [:development, :test]
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -1,8 +1,12 @@
 require 'google/apis/drive_v3'
 require 'googleauth'
+require 'json'
+require 'dotenv/load' # Charge les variables depuis .env
+
+credentials = JSON.parse(ENV['GOOGLE_DRIVE_CREDENTIALS'])
 
 DRIVE_SERVICE = Google::Apis::DriveV3::DriveService.new
 DRIVE_SERVICE.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: File.open('config/google_drive_service_account.json'),
+  json_key_io: StringIO.new(credentials.to_json),
   scope: ['https://www.googleapis.com/auth/drive']
 )
