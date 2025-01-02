dacdd25a55563be63b864101add776c9c50eaf7b
matinou-github <hadilouidrissou@gmail.com>
2025-01-02 11:17:53 +0100

    change drive code1

--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -1,26 +1,26 @@
 require 'google/apis/drive_v3'
 require 'googleauth'
 require 'json'
-require 'dotenv/load' # Charge les variables depuis .env
+require 'dotenv/load' # Charge les variables d'environnement depuis .env
 
 # Charge les informations d'identification JSON depuis la variable d'environnement
-credentials = JSON.parse(ENV['GOOGLE_DRIVE_CREDENTIALS'] || '{}')
-
-client_email = google_drive_credentials['client_email']
-private_key = google_drive_credentials['private_key']
+begin
+  google_drive_credentials = JSON.parse(ENV['GOOGLE_DRIVE_CREDENTIALS'])
+rescue JSON::ParserError => e
+  raise "Erreur de parsing des credentials Google Drive : #{e.message}. Vérifiez le format de la variable GOOGLE_DRIVE_CREDENTIALS."
+end
 
 # Crée un service Google Drive
 DRIVE_SERVICE = Google::Apis::DriveV3::DriveService.new
 
 # Autorisation via les informations d'identification de service
 DRIVE_SERVICE.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: StringIO.new(credentials.to_json),  # Passe les informations d'identification sous forme de JSON
-  scope: ['https://www.googleapis.com/auth/drive']  # Définissez les autorisations requises
+  json_key_io: StringIO.new(google_drive_credentials.to_json), # Passe les informations d'identification sous forme de JSON
+  scope: ['https://www.googleapis.com/auth/drive'] # Définissez les autorisations requises
 )
 
 # Test pour vérifier la connexion
 begin
-  # Demander la liste des fichiers pour vérifier si l'autorisation fonctionne
   response = DRIVE_SERVICE.list_files(page_size: 10)
   puts "Fichiers récupérés avec succès:"
   response.files.each { |file| puts "#{file.name} (#{file.id})" }
