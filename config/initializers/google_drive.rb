33c4a2160738967bc83af86e638eda372a672fe9
matinou-github <hadilouidrissou@gmail.com>
2025-01-02 11:27:08 +0100

    change drive code2

--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -1,29 +1,30 @@
 require 'google/apis/drive_v3'
 require 'googleauth'
 require 'json'
-require 'dotenv/load' # Charge les variables d'environnement depuis .env
+require 'dotenv/load' # Charge les variables d'environnement
 
-# Charge les informations d'identification JSON depuis la variable d'environnement
+# Vérifiez si GOOGLE_DRIVE_CREDENTIALS est définie
+if ENV['GOOGLE_DRIVE_CREDENTIALS'].nil? || ENV['GOOGLE_DRIVE_CREDENTIALS'].strip.empty?
+  raise "La variable d'environnement GOOGLE_DRIVE_CREDENTIALS est absente ou vide."
+end
+
 begin
+  # Charge et parse le contenu de GOOGLE_DRIVE_CREDENTIALS
   google_drive_credentials = JSON.parse(ENV['GOOGLE_DRIVE_CREDENTIALS'])
 rescue JSON::ParserError => e
-  raise "Erreur de parsing des credentials Google Drive : #{e.message}. Vérifiez le format de la variable GOOGLE_DRIVE_CREDENTIALS."
+  raise "Erreur de parsing du JSON dans GOOGLE_DRIVE_CREDENTIALS : #{e.message}"
 end
 
-# Crée un service Google Drive
+# Configure le service Google Drive
 DRIVE_SERVICE = Google::Apis::DriveV3::DriveService.new
 
-# Autorisation via les informations d'identification de service
-DRIVE_SERVICE.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: StringIO.new(google_drive_credentials.to_json), # Passe les informations d'identification sous forme de JSON
-  scope: ['https://www.googleapis.com/auth/drive'] # Définissez les autorisations requises
-)
-
-# Test pour vérifier la connexion
 begin
-  response = DRIVE_SERVICE.list_files(page_size: 10)
-  puts "Fichiers récupérés avec succès:"
-  response.files.each { |file| puts "#{file.name} (#{file.id})" }
-rescue Google::Apis::Error => e
-  puts "Erreur lors de l'appel à l'API Google Drive: #{e.message}"
+  DRIVE_SERVICE.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+    json_key_io: StringIO.new(ENV['GOOGLE_DRIVE_CREDENTIALS']),
+    scope: ['https://www.googleapis.com/auth/drive']
+  )
+rescue => e
+  raise "Erreur lors de la configuration des credentials Google Drive : #{e.message}"
 end
+
+puts "Service Google Drive configuré avec succès."
